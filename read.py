110129c105811fddfc43604611b9ec0cdab5c0f9
Jonny Fang <b00130117@gmail.com>
2023-02-07 20:14:30 +0800

    add filter good

--- read.py
+++ read.py
@@ -20,3 +20,11 @@ for d in data:
 		new.append(d)
 print('一共有', len(new),  '筆長度<100的留言')
 print(new[0])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
