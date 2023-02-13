6774204d25d0fb6071f5ae726905afc044b70196
Jonny Fang <b00130117@gmail.com>
2023-02-13 22:57:53 +0800

    add word account

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ with open('reviews.txt', 'r') as f: #打開txt檔
 		if count % 10000 == 0:       # % 求餘數
 			print(len(data))
 print('總共有', len(data), '筆資料')
-
+print(data[2])
 
 sum_d = 0
 for d in data:
@@ -31,3 +31,35 @@ print(good[0])
 
 bad = [d for d in data if 'bad' in d]   #快寫法 把有bad的留言裝進清單中
 print(bad)
+
+
+
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split()    #括號內預設為空白鍵
+	for word in words:
+		if word in wc:
+			wc[word] += 1 
+		else:
+			wc[word] = 1
+for word in wc:			
+	if wc[word] > 1000000:
+		print(word, wc[word])	
+print(len(wc))
+print(wc['Jonny'])	
+while True:
+	word = input('請問想查詢什麼字: ')
+	if word == 'q':
+		print('感謝使用!')
+		break
+	if word in wc:
+		print(word, '總共出現', wc[word], '次')
+	else:
+		print(word, '沒有出現過喔!')
+					
+
+
+
+
